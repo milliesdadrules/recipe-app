0bd4f7aa0ef9032dedf2e3a888b61e11481bde66
James McNeil <mcneil_james@hotmail.com>
2020-11-21 18:25:19 +0000

    removed recipes array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { v4 as uuidv4 } from 'uuid'
+import { loadRecipes } from './recipes'
 
-const recipes = [{
-    title: '',
-    summary: ''
-}]
 
-localStorage.setItem("recipes",JSON.stringify(recipes))
+// localStorage.setItem("recipes",JSON.stringify(recipes))
 
 document.querySelector('#add-recipe').addEventListener(('click'), (e) => {
     const recipeID = uuidv4()
