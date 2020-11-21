e40f3c716ede1c8894a944849a73772187a44796
James McNeil <mcneil_james@hotmail.com>
2020-11-21 18:47:05 +0000

    added save, get,  load functions & exported

--- src/recipes.js
+++ src/recipes.js
@@ -1,15 +1,25 @@
+import {v4 as uuidv4} from 'uuid'
+let recipes = []
+
+// Read any existing recipes from localStorage (recipes)
 const loadRecipes = () =>{
-    const storedRecipes = localStorage.getItem('recipes')
-    if(recipes){
-        return recipes = JSON.parse(storedRecipes)
-    } else {
-        return {
-            id: '',
-            title: '',
-            summary: '',
-            ingredients: ''
-        }
+    const recipesJSON = localStorage.getItem('recipes')
+    try {
+        recipes = recipesJSON ? JSON.parse(recipesJSON) : []
+    } catch (error){
+        recipes = []
     }
 }
 
-export { loadRecipes}
+// Saves new recipes to localStorage (recipes)
+const saveRecipes = () =>{
+    localStorage.setItem('recipes',JSON.stringify(recipes))
+}
+
+// Funtion to expose the recipes
+const getRecipes = () => recipes
+
+// call to initially load recipes
+loadRecipes()
+
+export { loadRecipes, saveRecipes, getRecipes }
